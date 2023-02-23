2b8a962206c92504460161e7ba8b824ac05a1d24
Colm Murphy <116750963+cmurphy012@users.noreply.github.com>
2023-02-23 14:32:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
 
-
+#User guess version
 def guess(x):
+    #Generate a random number for the user to try to guess
     random_number = random.randint(1, x)
+    
     guess = 0
+    #Loop until user guesses correctly
     while guess != random_number:
+        #Take user guess as input and compare to random number, respond accordingly
         guess = int(input(f"Guess a number between 1 and {x}: "))
         if guess == random_number:
             print("Correct!")
@@ -16,18 +20,19 @@ def guess(x):
             print("Too high!")
             continue
 
-
+#Computer guess version
 def comp_guess(x):
     low = 1
     high = x
     feedback = ""
-
+    #Loop until guess is correct
     while feedback != "C":
         if low != high:
+            #Generate a random number between min and max possible answer as guess
             guess = random.randint(low, high)
         else:
             guess = low
-
+        #Take feedback from user to inform next guess, continue until correct number is guessed
         feedback = input(f"Is {guess} too high (h), too low (l) or correct (c)\n").upper()
 
         if feedback == "H":
@@ -36,6 +41,3 @@ def comp_guess(x):
             low = guess + 1
 
     print(f"Correct! The number was {guess}")
-
-
-comp_guess(1000)
